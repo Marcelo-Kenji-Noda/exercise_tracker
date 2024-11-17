94834683a3a2d1923e69ab3de4f5d84532f9eb9c
Marcelo-Kenji-Noda <nodamarcelokenjinoda@gmail.com>
2024-11-17 20:46:14 -0300

    Atualizado lazy calls

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { CssBaseline, ThemeProvider, Toolbar, createTheme } from '@mui/material';
+import { Suspense, lazy } from 'react';
 
+import HomePage from './pages/HomePage';
+// import Statistics from './pages/Statistics';
 import { StrictMode } from 'react';
 import TopAppBar from './components/TopAppBar';
 import { createRoot } from 'react-dom/client';
-import { lazy } from 'react';
 
-const HomePage = lazy(() => import('./pages/HomePage'));
+//const HomePage = lazy(() => import('./pages/HomePage'));
+//const ExerciseSet = lazy(() => import('./pages/ExerciseSet'));
+//const ExercisePage = lazy(() => import('./pages/ExercisePage'));
+const Statistics = lazy(() => import('./pages/Statistics'));
 const ExerciseSet = lazy(() => import('./pages/ExerciseSet'));
 const ExercisePage = lazy(() => import('./pages/ExercisePage'));
-const Statistics = lazy(() => import('./pages/Statistics'));
-
 
 // Cria o tema com a fonte Roboto
 const theme = createTheme({
@@ -28,9 +31,9 @@ createRoot(document.getElementById('root')!).render(
         <Toolbar sx={{marginBottom:0.2}}/>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/exercise" element={<ExercisePage />} />
-          <Route path="/exerciseset" element={<ExerciseSet />} />
-          <Route path="/statistics" element={<Statistics />}/>
+          <Route path="/exercise" element={<Suspense fallback={<p>Loading...</p>}><ExercisePage /></Suspense>} />
+          <Route path="/exerciseset" element={<Suspense fallback={<p>Loading...</p>}><ExerciseSet /></Suspense>} />
+          <Route path="/statistics" element={<Suspense fallback={<p>Loading...</p>}><Statistics /></Suspense>}/>
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
