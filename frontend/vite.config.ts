511c83fcd55134d6aa03630522b559f449628f7f
Marcelo-Kenji-Noda <nodamarcelokenjinoda@gmail.com>
2024-11-18 12:59:31 -0300

    removed plugins

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
-import viteCompression from 'vite-plugin-compression';
-
 export default defineConfig({
   plugins: [
-    react(),
-    viteCompression({
-      algorithm: 'gzip', // Compressão gzip (padrão)
-      ext: '.gz',        // Extensão para arquivos gerados
-      deleteOriginFile: false, // Mantenha o arquivo original
-    }),
-    visualizer({ open: true,
-      filename: 'stats.html', // Nome do arquivo gerado
-      template: 'treemap', // Formato de visualização
-    }), // Abre a análise no navegador após o build
+    react()
   ],
   server: {
     proxy: {
